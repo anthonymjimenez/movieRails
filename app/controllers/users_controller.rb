fd3919a65bca4400655c172f03d2022ef1d5bfe9
anthonymjimenez <anthonyjim96@gmail.com>
2020-10-19 22:21:06 -0400

    created prototype show and index for movies, added helper methods to create symbols from API response" ;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+                    
+
     def update
         @user = User.find(params[:id])
         if @user.update
